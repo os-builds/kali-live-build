dc91594da8be02971cc4c42cb7469f5e33c05b75
Nico Felbinger <26925347+felbinger@users.noreply.github.com>
2022-08-29 13:57:48 +0200

    improved several things (#8)
    * improved several things
    
    * moved post-install for testing purpose to /lib/live/config/0100-post-install; completed openvas installation; added kernel parameter nozsh to every live boot option
    
    * moved openvas setup steps to script which runs on live system
    
    * add kali-linux-large

--- config/includes.chroot/etc/skel/post-install.sh
+++ config/includes.chroot/etc/skel/post-install.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
 # change wallpaper
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/custom/blackscreen.png
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set /usr/share/backgrounds/custom/blackscreen.png
+
+# change wallpaper on live system running inside a virtual machine
+#xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitorVirtual1/workspace0/last-image --set /usr/share/backgrounds/custom/blackscreen.png
 
 # adjust keybinds (add Super + E for file manager)
-if ! which xmlstarlet &> /dev/null; then
-  sudo apt install -y xmlstarlet
-fi
-keyboardShortcutsConfig="~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml"
-xPath='/channel/property[@name="commands"]/property[@name="custom"]/property[not(@name)]'
-xmlstarlet ed --inplace --insert ${xPath} --type attr --name "name" --value "&lt;Super&gt;e" ${keyboardShortcutsConfig}
-xmlstarlet ed --inplace --insert ${xPath} --type attr --name "type" --value "string" ${keyboardShortcutsConfig}
-xmlstarlet ed --inplace --insert ${xPath} --type attr --name "value" --value "exo-open --launch FileManager" ${keyboardShortcutsConfig}
+# TODO not working
+xmlstarlet ed --inplace --subnode \
+  '/channel/property[@name="commands"]/property[@name="custom"]' \
+  --type elem --name "property" \
+  ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+
+xmlstarlet ed --inplace --insert \
+  '/channel/property[@name="commands"]/property[@name="custom"]/property[not(@name)]' \
+  --type attr --name "name" --value "<Super>e" \
+  ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+
+xmlstarlet ed --inplace --insert \
+  '/channel/property[@name="commands"]/property[@name="custom"]/property[@name="<Super>e"]' \
+  --type attr --name "type" --value "string" \
+  ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
+
+xmlstarlet ed --inplace --insert \
+  '/channel/property[@name="commands"]/property[@name="custom"]/property[@name="<Super>e"]' \
+  --type attr --name "value" --value "exo-open --launch FileManager" \
+  ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-keyboard-shortcuts.xml
